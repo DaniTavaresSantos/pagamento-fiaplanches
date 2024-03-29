287d748246f338c1a67e9849323e6e72c03aaa80
David Abreu <davidabreu.dev@gmail.com>
2024-03-29 14:01:28 +0000

    fase-5: config dockerfile

--- src/main/java/br/com/fiaplanchespayment/infra/config/beans/PaymentOrderConfig.java
+++ src/main/java/br/com/fiaplanchespayment/infra/config/beans/PaymentOrderConfig.java
@@ -3,6 +3,7 @@ package br.com.fiaplanchespayment.infra.config.beans;
 import br.com.fiaplanchespayment.application.ports.out.NotifyPaymentTopicPortOut;
 import br.com.fiaplanchespayment.application.ports.out.PaymentRepositoryPortOut;
 import br.com.fiaplanchespayment.application.usecases.PaymentOrderUseCase;
+import br.com.fiaplanchespayment.domain.PaymentValidatorPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +12,8 @@ public class PaymentOrderConfig {
 
     @Bean
     public PaymentOrderUseCase paymentOrderUseCase(PaymentRepositoryPortOut paymentRepositoryPortOut,
-                                                   NotifyPaymentTopicPortOut notifyPaymentTopicPortOut) {
-        return new PaymentOrderUseCase(paymentRepositoryPortOut, notifyPaymentTopicPortOut);
+                                                   NotifyPaymentTopicPortOut notifyPaymentTopicPortOut,
+                                                   PaymentValidatorPort paymentValidatorPort) {
+        return new PaymentOrderUseCase(paymentRepositoryPortOut, notifyPaymentTopicPortOut, paymentValidatorPort);
     }
 }
